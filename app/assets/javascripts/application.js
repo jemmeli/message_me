bf25908676314565440590747da82a0e0ff37b4a
jemmeli <jemmeli84@gmail.com>
2020-01-12 02:58:50 +0100

    add autoscroll

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,6 +17,22 @@
 //= require semantic-ui
 //= require_tree .
 
+
+scroll_bottom = function() {
+  if ($('#messages').length > 0) {
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+  }
+}
+
+submit_message = function() {
+  $('#message_body').on('keydown', function(e) {
+    if (e.keyCode == 13) {
+      $('button').click();
+      e.target.value = "";
+    };
+  });
+};
+
 $(document).on('turbolinks:load' , function() {
 
     $('.ui.dropdown').dropdown();
@@ -24,6 +40,9 @@ $(document).on('turbolinks:load' , function() {
     $('.message .close').on('click', function() {
 		$(this).closest('.message').transition('fade');
 	});
+
+	submit_message();
+  	scroll_bottom();
 
 })
 
